8cecaa61ce03723ddfabe1605db47cacc383320f
Ginger Korporaal <gingerkorporaal@hotmail.com>
2026-03-29 23:55:43 +0200

    responsive design exercise afgerond

--- exercises/10_styling-design-system-02-exercise/styling-design-system-02-exercise/src/App.jsx
+++ exercises/10_styling-design-system-02-exercise/styling-design-system-02-exercise/src/App.jsx
@@ -1,32 +1,28 @@
-import {
-    ResponsiveButton
-} from './components/ResponsiveButton';
-import { 
-    SimpleGrid
-} from '@chakra-ui/react'
+import { ResponsiveButton } from "./components/ResponsiveButton";
+import { SimpleGrid } from "@chakra-ui/react";
 
 export const App = () => {
-    // Do not change the below code
-    const buttons = [];
-    for (let i = 1; i <= 128; i++) {
-        buttons.push(
-            <ResponsiveButton key={i}>
-                Button #{i}
-            </ResponsiveButton>
-        );
-    }
+  // Do not change the below code
+  const buttons = [];
+  for (let i = 1; i <= 128; i++) {
+    buttons.push(<ResponsiveButton key={i}>Button #{i}</ResponsiveButton>);
+  }
 
-    // Adjust the <SimpleGrid> component such that it holds as many buttons per row as possible,
-    // without having the buttons turn red. Make use of the base, sm, md, lg and xl breakpoints.
-    return (
-        <SimpleGrid gap={4} padding={4} columns={{
-            base: 2,
-            sm: 3,
-            md: null,
-            lg: null,
-            xl: null
-        }}>
-            {buttons}
-        </SimpleGrid>
-    )
-}
+  // Adjust the <SimpleGrid> component such that it holds as many buttons per row as possible,
+  // without having the buttons turn red. Make use of the base, sm, md, lg and xl breakpoints.
+  return (
+    <SimpleGrid
+      gap={4}
+      padding={4}
+      columns={{
+        base: 2,
+        sm: 3,
+        md: 4,
+        lg: 5,
+        xl: 6,
+      }}
+    >
+      {buttons}
+    </SimpleGrid>
+  );
+};
